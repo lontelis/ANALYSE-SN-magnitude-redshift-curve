ce567232fb3f243380dc27983a3becf103c311ec
lontelis <31013256+lontelis@users.noreply.github.com>
2019-04-17 18:29:45 +0200

    Update analyse_SN.py

--- analyse_SN.py
+++ analyse_SN.py
@@ -2,7 +2,7 @@ import cosmology,fitting
 from numpy import *
 from pylab import *
 
-
+# Read data
 totoSN = loadtxt('data/supernovaUnion2011.txt',usecols=[1,2,3,4])
 z_unsorted  = totoSN[:,0]
 mu          = totoSN[:,1]
@@ -13,15 +13,14 @@ z  = z_unsorted[permutter]
 mu0 =mu[permutter]
 dmu0=dmu[permutter]
 
-#def model(x,pars): return pars[0]*log10(x)+pars[1]
-#res = fitting.dothefit(z,mu0,dmu0,[1.,40.],functname=model,method='minuit')
-#dl = 10**((model(z,res[1])-25 )/ 5.)
-
+#Define model
 def model2(x,pars): return log10( cosmology.get_dist(z,type='dl',params=[pars[0],pars[1],-1,0])/0.7)*5.0+25.
+
+#Do fit
 res = fitting.dothefit(z,mu0,dmu0,[.3,.7],functname=model2,method='minuit')
 
 
-
+# plot
 figure(1,figsize=(15,10)),clf()
 errorbar(z,mu0,yerr=dmu0,fmt='.', label='SN Union2.1 Suzuki et Al. 2011') #,label='Riess et Al. 1998 High-Z Templates')
 xscale('linear'),ylabel('$\mu(z)$',size=25),xlabel('$z$',size=25)
